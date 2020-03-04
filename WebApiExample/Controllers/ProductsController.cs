93ca345859f9ecf5370493a38a4bbdefd09678d7
yogesh-roy <61585256+yogesh-roy@users.noreply.github.com>
2020-03-04 12:46:31 +0530

    WebApi Products Page
    Products Page Added

---
+++ WebApiExample/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiExample.Models;
+
+namespace WebApiExample.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product{Id=1,Name="Dell",Category="Laptop",Price= 20000},
+            new Product{Id=2,Name="LG",Category="TV",Price= 30000},
+            new Product{Id=3,Name="Apple",Category="Mobile",Price= 40000},
+            new Product{Id=4,Name="Dell",Category="Laptop",Price= 20000}
+        };
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
